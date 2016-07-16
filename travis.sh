c72cc051a6a383ea51dc7444714cc1f7c82caa22
Stefan Eike <stefan.eike@mailbox.org>
2016-07-16 18:11:46 +0200

    Fix Travis script.

--- travis.sh
+++ travis.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-
-rm org.doctales.terminology
-rm -R dita-ot-2.3.1
-
 zip -r org.doctales.terminology.zip . -x *.zip* *.git/* *temp/* *out/*
-#zip -r org.doctales.terminology.zip .
-#curl -LO https://github.com/dita-ot/dita-ot/releases/download/2.3.1/dita-ot-2.3.1.zip
+curl -LO https://github.com/dita-ot/dita-ot/releases/download/2.3.1/dita-ot-2.3.1.zip
 unzip -q dita-ot-2.3.1.zip
 chmod +x dita-ot-2.3.1/bin/dita
 dita-ot-2.3.1/bin/dita -install org.doctales.terminology.zip
