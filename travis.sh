6b695eceb710d4947245ed8a164d96cf11c4ea13
Stefan Eike <stefan.eike@mailbox.org>
2017-01-15 13:49:12 +0100

    Add missing semantic net element
    Fix class attribute
    
    
    Update catalog
    
    
    Update build script
    
    
    Update build script and integrate org.dita-ng.library
    
    
    Continue
    
    
    continue
    
    
    continue
    
    
    continue
    
    
    continue
    
    
    Migrate termentry to RNG
    
    
    Temporarily remove MathML
    
    
    Fix some issues
    
    
    Migrate semantic net to RelaxNG

--- travis.sh
+++ travis.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 zip -r org.doctales.terminology.zip . -x *.zip* *.git/* *temp/* *out/*
-curl -LO https://github.com/dita-ot/dita-ot/releases/download/2.4.1/dita-ot-2.4.1.zip
-unzip -q dita-ot-2.4.1.zip
+curl -LO https://github.com/dita-ot/dita-ot/releases/download/2.4.2/dita-ot-2.4.2.zip
+unzip -q dita-ot-2.4.2.zip
 mv dita-ot-*/ dita-ot/
+git clone https://github.com/queshaw/dita-ng-library.git
+cd dita-ng-library
+./gradlew
+cd ..
 chmod +x dita-ot/bin/dita
+dita-ot/bin/dita -install dita-ng-library/dist/org.dita-ng.library-0.1.zip
 dita-ot/bin/dita -install org.doctales.terminology.zip
 dita-ot/bin/dita -input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap -format termchecker-dita -verbose -Dprocessing-mode=strict -Dargs.language=en-GB -o out/termchecker-dita
 dita-ot/bin/dita -input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap -format termchecker-xliff -verbose -Dprocessing-mode=strict -Dargs.language=en-GB -o out/termchecker-xliff
