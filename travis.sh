f21494cfac0ff80aa5d9a8ee44e67426d1a26ddf
Stefan Eike <stefan.eike@mailbox.org>
2017-10-27 21:21:50 +0200

    Refactoring and rename termbrowser to termbrowser-classic

--- travis.sh
+++ travis.sh
@@ -7,11 +7,12 @@ git clone https://github.com/queshaw/dita-ng-library.git
 cd dita-ng-library
 ./gradlew
 cd ..
+chmod +x dita-ot/bin/ant
 chmod +x dita-ot/bin/dita
-dita-ot/bin/dita -install dita-ng-library/dist/org.dita-ng.library-0.1.zip
-dita-ot/bin/dita -install org.doctales.terminology.zip
-dita-ot/bin/dita -input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap -format termchecker-dita -verbose -Dprocessing-mode=strict -Dargs.language=en-GB -o out/termchecker-dita
-dita-ot/bin/dita -input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap -format termchecker-xliff -verbose -Dprocessing-mode=strict -Dargs.language=en-GB -o out/termchecker-xliff
-dita-ot/bin/dita -input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap -format tbx-basic -verbose -Dprocessing-mode=strict -o out/tbx-basic
-dita-ot/bin/dita -input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap -format tbx-min -verbose -Dprocessing-mode=strict -Dargs.source.language=en-GB -Dargs.target.language=de-DE -o out/tbx-min
-dita-ot/bin/dita -input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap -format termbrowser-html5 -verbose -Dargs.default.language=en-GB -o out/termbrowser-html5
+dita-ot/bin/dita --install dita-ng-library/dist/org.dita-ng.library-0.1.zip
+dita-ot/bin/dita --install org.doctales.terminology.zip
+dita-ot/bin/dita --input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap --format termchecker-dita --verbose -Dprocessing-mode=strict -Dargs.language=en-GB --output out/termchecker-dita
+dita-ot/bin/dita --input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap --format termchecker-xliff --verbose -Dprocessing-mode=strict -Dargs.language=en-GB --output out/termchecker-xliff
+dita-ot/bin/dita --input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap --format tbx-basic --verbose -Dprocessing-mode=strict --output out/tbx-basic
+dita-ot/bin/dita --input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap --format tbx-min --verbose -Dprocessing-mode=strict -Dargs.source.language=en-GB -Dargs.target.language=de-DE --output out/tbx-min
+dita-ot/bin/dita --input dita-ot/plugins/org.doctales.terminology/samples/terminology.ditamap --format termbrowser-html5 --verbose -Dargs.default.language=en-GB --output out/termbrowser-html5
