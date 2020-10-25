206fe6d039e4e0bdc6cf47f432821c132d59eb8d
Stefan Eike <stefan.eike@mailbox.org>
2020-10-25 19:46:12 +0100

    Remove legacy termbrowser target from build script

--- travis.sh
+++ travis.sh
@@ -13,7 +13,6 @@ dita-ot-2.5.4/bin/dita --input dita-ot-2.5.4/plugins/org.doctales.terminology/sa
 dita-ot-2.5.4/bin/dita --input dita-ot-2.5.4/plugins/org.doctales.terminology/samples/terminology.ditamap --format termchecker-xliff -Dprocessing-mode=strict -Dargs.language=en-GB -Dargs.check.elements=both --output out/termchecker-xliff --verbose
 dita-ot-2.5.4/bin/dita --input dita-ot-2.5.4/plugins/org.doctales.terminology/samples/terminology.ditamap --format tbx-basic -Dprocessing-mode=strict --output out/tbx-basic --verbose
 dita-ot-2.5.4/bin/dita --input dita-ot-2.5.4/plugins/org.doctales.terminology/samples/terminology.ditamap --format tbx-min -Dprocessing-mode=strict -Dargs.source.language=en-GB -Dargs.target.language=de-DE --output out/tbx-min --verbose
-dita-ot-2.5.4/bin/dita --input dita-ot-2.5.4/plugins/org.doctales.terminology/samples/terminology.ditamap --format termbrowser-html5 -Dargs.default.language=en-GB --output out/termbrowser-html5 --verbose
 
 # DITA-OT 3.5.4
 curl -LO https://github.com/dita-ot/dita-ot/releases/download/3.5.4/dita-ot-3.5.4.zip
